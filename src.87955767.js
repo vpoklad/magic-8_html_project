1d78ad797da70437a78aa884683a12ef5a42ef99
vpoklad <78758930+vpoklad@users.noreply.github.com>
2021-05-16 20:20:21 +0000

    Deploying to gh-pages from @ vpoklad/magic-8_html_project@df84b09882256009f5f1f50894571f55ce124275 🚀

--- src.87955767.js
+++ src.87955767.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"/home/runner/work/magic-8_html_project/magic-8_html_project/src/images/coffee-cup-bullet.svg":[["coffee-cup-bullet.a88cbea5.svg","RFbK"],"RFbK"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/magic-8_html_project/src.781a78e5.js.map
+//# sourceMappingURL=/magic-8_html_project/src.87955767.js.map
